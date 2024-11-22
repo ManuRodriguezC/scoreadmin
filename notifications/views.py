418e55751462a99406cc70466f317bf88fa74dca
ManuRodriguezC <estebanclimb@gmail.com>
2024-11-22 15:50:16 -0500

    Update views for apis

--- notifications/views.py
+++ notifications/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from datetime import datetime
+from django.utils import timezone
 from .models import *
 from .serializer import EventsSerializer, NewsSerializer
 
 class EventsApiView(viewsets.ModelViewSet):
     serializer_class = EventsSerializer
-    current_date = datetime.now()
+    current_date = timezone.make_aware(datetime.now())
     queryset = Event.objects.filter(date__gte=current_date)
     http_method_names = ['get']
 
